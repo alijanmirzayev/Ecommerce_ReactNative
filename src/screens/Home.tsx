5703aa9cc0563a91dce4b828eb78eb2172e08b40
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-06-03 20:35:55 +0400

    updated Home and Detail screens

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,94 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TextInput, StyleSheet, TouchableOpacity, FlatList, Dimensions } from 'react-native'
+import React, { useState } from 'react'
+import { CATEGORIES } from '../data/categories'
+import { COLORS } from '../constants/Colors'
+import { FONTS } from '../constants/Fonts'
+import Search from '../assets/Icons/Search'
+import CategoryItem from '../components/CategoryItem'
+import ProductItem from '../components/ProductItem'
 
-export default function Home() {
+const screenHeight = Dimensions.get('screen').height
+const header = screenHeight / 4
+const product = screenHeight - header
+
+export default function Home({navigation}: any) {
+  const [selecetedCat, setSelectedCat] = useState<Number>(1)
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+
+        <View style={styles.headerContainer}>
+
+          <View style={styles.searchContainer}>
+            <TextInput placeholder='Search' placeholderTextColor='#868686' style={styles.search} />
+            <View style={{ position: 'absolute', top: 14, left: 18 }}>
+              <Search />
+            </View>
+          </View>
+
+          <View style={styles.headerTextContainer}>
+            <Text style={styles.headerText}>Order Online</Text>
+            <Text style={styles.headerText}>collect in store</Text>
+          </View>
+
+        </View>
+
+        <View style={styles.otherContainer}>
+
+          <View style={styles.categoryContainer}>
+            <CategoryItem selecetedCat={selecetedCat} setSelectedCat={setSelectedCat} />
+          </View>
+
+          <View style={styles.productContainer}>
+            <ProductItem selecetedCat={selecetedCat} navigation={navigation}/>
+          </View>
+
+        </View>
+
+      </SafeAreaView>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.LIGHT.background,
+    flex: 1
+  },
+  headerContainer: {
+    height: screenHeight / 4,
+  },
+  otherContainer: {
+    height: product,
+  },
+  searchContainer: {
+    marginHorizontal: 18,
+    marginTop: 46,
+    borderWidth: 1,
+    borderColor: '#C9C9C9',
+    borderRadius: 30
+  },
+  headerTextContainer: {
+    marginTop: 22,
+    marginHorizontal: 18,
+  },
+  categoryContainer: {
+    marginHorizontal: 18,
+  },
+  productContainer: {
+    marginTop: 24,
+    marginLeft: 24
+  },
+  search: {
+    color: '#868686',
+    fontSize: 17,
+    fontFamily: FONTS.Regular,
+    marginLeft: 48
+  },
+  headerText: {
+    color: COLORS.LIGHT.title,
+    fontFamily: FONTS.Bold,
+    fontSize: 34
+  }
+})
--- src/screens/ProductDetail.tsx
+++ src/screens/ProductDetail.tsx
@@ -1,10 +1,143 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import ArrowLeft from '../assets/Icons/ArrowLeft'
+import Heart from '../assets/Icons/Heart'
+import { COLORS } from '../constants/Colors'
+import { FONTS } from '../constants/Fonts'
+import ColorItem from '../components/ColorItem'
 
-export default function ProductDetail() {
+export default function ProductDetail({ route, navigation }: any) {
+
+  const redirectBasket = (data: any) => {
+    navigation.navigate('Basket', data)
+  }
+
   return (
-    <View>
-      <Text>ProductDetail</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+
+        <View style={styles.headerContainer}>
+          <ArrowLeft />
+          <Heart />
+        </View>
+
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{ uri: route.params.image }} />
+        </View>
+
+        <View style={styles.detailContainer}>
+
+          <Text style={styles.title}>{route.params.brand} {route.params.model}</Text>
+
+          <View>
+            <Text style={styles.colorsText}>Colors</Text>
+
+            <ColorItem data={route.params} />
+
+            <Text style={styles.description}>{route.params.description}</Text>
+          </View>
+
+          <View style={styles.priceBasketContainer}>
+            <View style={styles.priceContainer}>
+              <Text style={styles.priceText}>Price</Text>
+              <Text style={styles.price}>$ {route.params.price}</Text>
+            </View>
+
+            <TouchableOpacity onPress={() => redirectBasket(route.params)} style={styles.btnContainer}>
+              <Text style={styles.btnText}>Add to Basket</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
+
+      </SafeAreaView>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    height: '100%',
+    width: '100%'
+  },
+  headerContainer: {
+    marginTop: 12,
+    marginHorizontal: 18,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  imageContainer: {
+    marginTop: 12,
+    height: '50%',
+  },
+  detailContainer: {
+    marginTop: 12,
+    height: '40%',
+    marginHorizontal: 18,
+  },
+  priceBasketContainer: {
+    justifyContent: 'flex-end',
+    flex: 1,
+  },
+  priceContainer: {
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  btnContainer: {
+    marginHorizontal: 16,
+    marginTop: 12,
+    backgroundColor: '#5956E9',
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  },
+  title: {
+    fontFamily: FONTS.Bold,
+    color: COLORS.LIGHT.title,
+    fontSize: 28,
+  },
+  colorsText: {
+    marginTop: 8,
+    fontFamily: FONTS.Bold,
+    color: COLORS.LIGHT.title,
+    fontSize: 19,
+  },
+  description: {
+    marginTop: 12,
+    fontFamily: FONTS.Regular,
+    color: 'gray',
+    fontSize: 16,
+  },
+  priceText: {
+    fontFamily: FONTS.Regular,
+    color: COLORS.LIGHT.title,
+    fontSize: 17,
+  },
+  price: {
+    fontFamily: FONTS.ExtraBold,
+    color: '#5956E9',
+    fontSize: 24,
+  },
+  btnText: {
+    fontSize: 20,
+    fontFamily: FONTS.Bold,
+    color: COLORS.LIGHT.description,
+    textAlign: 'center',
+    paddingHorizontal: 12,
+    paddingVertical: 25,
+  }
+})
