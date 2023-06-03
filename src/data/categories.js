b72e959a90ce6fd382e1a8f3cb04c7be9579c20e
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-06-03 20:36:07 +0400

    added static data file

---
+++ src/data/categories.js
@@ -0,0 +1,18 @@
+export const CATEGORIES = [
+    {
+        id: 1,
+        name: 'Wearable'
+    },
+    {
+        id: 2,
+        name: 'Laptops'
+    },
+    {
+        id: 3,
+        name: 'Phones'
+    },
+    {
+        id: 4,
+        name: 'Drones'
+    }
+]
---
+++ src/data/products.js
@@ -0,0 +1,32 @@
+export const PRODUCTS = [
+    {
+        id: 1,
+        categoryId: 1,
+        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/FG123?wid=2000&hei=2000&fmt=jpeg',
+        brand: 'Apple Watch',
+        model: 'Series 6.',
+        description: 'Apple Watch Series 6 delivers breakthrough wellness and fitness capabilities',
+        price: 359,
+        colors: ['red']
+    },
+    {
+        id: 2,
+        categoryId: 1,
+        image: 'https://www.bakuelectronics.az/assets/images/products/84243/samsung-galaxy-watch-active2-44mm-alum-black-1.jpg',
+        brand: 'Samsung Watch',
+        model: 'Galaxy Watch Active2',
+        description: 'Stay connected with a lifestyle and health partner on your wrist that gives you advice in the moment.',
+        price: 349,
+        colors: ['blue', 'black']
+    },
+    {
+        id: 3,
+        categoryId: 1,
+        image: 'https://m.media-amazon.com/images/I/71YdE55GwjL._AC_UF1000,1000_QL80_.jpg',
+        brand: 'Apple Watch',
+        model: 'SE 2',
+        description: 'The Apple Watch SE 2 is arguably the best Apple Watch for most people, as it’s a lot more affordable than the Apple Watch 8, yet has most of that wearable’s core features and even the same chipset.',
+        price: 299,
+        colors: ['red', 'blue', 'green']
+    }
+]
